6462a63a39e7f7cb49a1929228429dc294fc5678
Alexander Ikonomidis <xikonomidis@gmail.com>
2014-01-26 13:21:16 -0500

    Figuring stuff out

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
-// Define margins
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right;
-    height = 500 - margin.top - margin.bottom;
-
-//Create scale functions
-var xScale = d3.scale.linear()
-    .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-    .range([0, width]);
-var yScale = d3.scale.linear()
-    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-    .range([height, 0]);
-
-//Define Axes
-var xaxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom")
-    .ticks(10);
-var yaxis = d3.svg.axis()
-    .scale(yScale)
-    .orient("left")
-    .ticks(10);
+var svg;
 
 /**
 THIS FUNCTION IS CALLED WHEN THE WEB PAGE LOADS. PLACE YOUR CODE TO LOAD THE
@@ -29,41 +8,63 @@ DIV ON THE PAGE.
 This function is passed the variables to initially draw on the x and y axes.
 **/
 function init(xAxis, yAxis){
-    d3.tsv("data/data.csv", type, function(error, data) {
-           var svg = d3.select("#vis").append("svg")
-                .attr("width", width+margin.left+margin.right)
-                .attr("height", height+margin.top+margin.bottom)
-                .append("g")
-                    .attr("transform", "translate("+margin.left+","+margin.top+")");
+    xdata = xAxis;
+    ydata = yAxis;
+    
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+    
+    var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+    
+    var y = d3.scale.linear()
+    .range([height, 0]);
+    
+    var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+    
+    var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10, "%");
+    
+    svg = d3.select("#vis").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+   /* d3.csv("data/data.csv", type, function(error, data) {
+           x.domain(data.map(function(d) { return d.letter; }));
+           y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
            
-           x.domain(data.map(function(d){return d.xAxis}));
-           y.domain([0,d3.max(data, function(){return d.yAxis;}]);
-                
-           // Create Axes
            svg.append("g")
-                .attr("class", "x axis")
-                .attr("tranfrom", "translate(0,"+height+")")
-                .call(xaxis);
-           svg.append("g")
-                .attr("class", "y axis")
-                .call(yaxis)
-                .append("text")
-                     .attr("transform", "rotate(-90)")
-                     .attr("y", 6)
-                     .attr("dy", ".71em")
-                     .style("text-anchor", "end")
-                     .text(yAxis);
+           .attr("class", "x axis")
+           .attr("transform", "translate(0," + height + ")")
+           .call(xAxis);
            
-           // Create Points
-           svg.selectAll("circle")
-                .data(data)
-                .enter()
-                .append("circle")
-                .attr("cx", function(d) { return xScale(d.xAxis); }) // ?
-                .attr("cy", function(d) { return yScale(d.yAxis); }) // ?
-                .attr("r", 5; })
-                .on("click", function(d){console.log("clicked on" + d.xAxis);}); // ?
-    });
+           svg.append("g")
+           .attr("class", "y axis")
+           .call(yAxis)
+           .append("text")
+           .attr("transform", "rotate(-90)")
+           .attr("y", 6)
+           .attr("dy", ".71em")
+           .style("text-anchor", "end")
+           .text("Frequency");
+           
+           svg.selectAll(".bar")
+           .data(data)
+           .enter().append("rect")
+           .attr("class", "bar")
+           .attr("x", function(d) { return x(d.letter); })
+           .attr("width", x.rangeBand())
+           .attr("y", function(d) { return y(d.frequency); })
+           .attr("height", function(d) { return height - y(d.frequency); });
+           
+           });*/
 }
 
 /**
@@ -73,7 +74,8 @@ x axis changes. It is passed the variable name that has been selected, such as
 "compactness". Populate this function to update the scatterplot accordingly.
 **/
 function onXAxisChange(value){
-
+    console.log("on x axis change");
+    init(value, ydata);
 }
 
 
@@ -96,4 +98,9 @@ a string and it will be displayed. For example,
 **/
 function showDetails(string){
     d3.select('#details').html(string);
+}
+
+function type(d) {
+    d.frequency = +d.frequency;
+    return d;
 }
