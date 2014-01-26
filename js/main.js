5abf62380e86f0b5c7144f4a539d31b432f5a1c0
Ashu Gupta <ashutoshgupta2k12@gmail.com>
2014-01-26 18:11:33 -0500

    Added DetailOnDemand Functionality & Legends

--- js/main.js
+++ js/main.js
@@ -1,126 +1,157 @@
-
-var xAxisData, yAxisData
+var x, y;   //Used in changing axis variables
+var varities = new Array("Kama", "Rosa", "Canadian");
+var cData = [], klData = [], kwData = [], aData = [], gData = [], vData = [];
 /**
-THIS FUNCTION IS CALLED WHEN THE WEB PAGE LOADS. PLACE YOUR CODE TO LOAD THE
-DATA AND DRAW YOUR VISUALIZATION HERE. THE VIS SHOULD BE DRAWN INTO THE "VIS" 
-DIV ON THE PAGE.
+ THIS FUNCTION IS CALLED WHEN THE WEB PAGE LOADS. PLACE YOUR CODE TO LOAD THE
+ DATA AND DRAW YOUR VISUALIZATION HERE. THE VIS SHOULD BE DRAWN INTO THE "VIS"
+ DIV ON THE PAGE.
+ 
+ This function is passed the variables to initially draw on the x and y axes.
+ **/
 
-This function is passed the variables to initially draw on the x and y axes.
-**/
-
-///fjdiopfjaasd
 function init(xAxis, yAxis){
-    xAxisData = xAxis;
-    yAxisData = yAxis;
-    var dataset = [];
+    x = xAxis; y = yAxis;   //Set globals
+    
     d3.csv("data/data.csv", function(data) {
-           dataset = data.map(function(d) { return [ +d[xAxis], +d[yAxis] ]; });
-           var xdata = data.map(function(d){ return +d[xAxis];});
-           var ydata = data.map(function(d){ return +d[yAxis];});
-           console.log(dataset);
+           var xData = data.map(function(d){ return +d[xAxis]; });      //Load data from data.csv
+           var yData = data.map(function(d){ return +d[yAxis]; });
+           cData = data.map(function(d){ return +d["compactness"]});
+           klData = data.map(function(d){ return +d["kernelLength"]});
+           kwData = data.map(function(d){ return +d["kernelWidth"]});
+           aData = data.map(function(d){ return +d["asymmetryCoefficient"]});
+           gData = data.map(function(d){ return +d["grooveLength"]});
+           vData = data.map(function(d){ return d["variety"]});
            
-           // data that you want to plot, I've used separate arrays for x and y values
-           //var xdata = [5, 10, 15, 20], ydata = [3, 17, 4, 6];
+           var margin = {top:20, right:15, bottom:60, left:60};         //Create Margins
+           var width = 960 - margin.left - margin.right;
+           var height = 500 - margin.top - margin.bottom;
            
-           console.log(xdata);
-           console.log(ydata);
+           var xr = d3.max(xData) - d3.min(xData);                      //Create XY Scale Functions
+           var yr = d3.max(yData) - d3.min(yData);
+           var xScale = d3.scale.linear()
+                .domain([d3.min(xData) - xr/15.0, d3.max(xData) + xr/15.0]) //from xData range
+                .range([0, width]);                                         //to axis length
+           var yScale = d3.scale.linear()
+                .domain([d3.min(yData) - yr/15.0, d3.max(yData) + yr/15.0])
+                .range([height, 0]);
            
-           // size and margins for the chart
-           var margin = {top: 20, right: 15, bottom: 60, left: 60}
-           , width = 960 - margin.left - margin.right
-           , height = 500 - margin.top - margin.bottom;
-           
-           // x and y scales, I've used linear here but there are other options
-           // the scales translate data values to pixel values for you
-           var range = d3.max(xdata)-d3.min(xdata);
-           var x = d3.scale.linear()
-           .domain([d3.min(xdata)-range/15.0, d3.max(xdata)+range/15.0])  // the range of the values to plot
-           .range([ 0, width ]);        // the pixel range of the x-axis
-           
-           range = d3.max(ydata)-d3.min(ydata);
-           var y = d3.scale.linear()
-           .domain([d3.min(ydata)-range/15.0, d3.max(ydata)+range/15.0])
-           .range([ height, 0 ]);
-           
-           // the chart object, includes all margins
-           d3.select('svg').remove();
-           var chart = d3.select('#vis')
-           .append('svg:svg')
-           .attr('width', width + margin.right + margin.left)
-           .attr('height', height + margin.top + margin.bottom)
-           .attr('class', 'chart')
-           
-           // the main object where the chart and axis will be drawn
-           var main = chart.append('g')
-           .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-           .attr('width', width)
-           .attr('height', height)
-           .attr('class', 'main')
-           
-           // draw the x axis
-           var xaxis = d3.svg.axis()
-           .scale(x)
-           .orient('bottom');
-           
-           main.append('g')
-           .attr('transform', 'translate(0,' + height + ')')
-           .attr('class', 'main axis date')
-           .call(xaxis);
-           
-           // draw the y axis
+           var xaxis = d3.svg.axis()                                    //Create XY Axes
+                .scale(xScale)
+                .orient("bottom");
            var yaxis = d3.svg.axis()
-           .scale(y)
-           .orient('left');
+                .scale(yScale)
+                .orient("left");
            
-           main.append('g')
-           .attr('transform', 'translate(0,0)')
-           .attr('class', 'main axis date')
-           .call(yaxis);
+           d3.select("svg").remove();                                   //Clear previous SVG plot
            
-           // draw the graph object
-           var g = main.append("svg:g");
+           var svg = d3.select("#vis")                                  //Create SVG
+                .append("svg")
+                .attr("width", width + margin.right + margin.left)
+                .attr("height", height + margin.top + margin.bottom);
            
-           g.selectAll("scatter-dots")
-           .data(ydata)  // using the values in the ydata array
-           .enter().append("svg:circle")  // create a new circle for each value
-           .attr("cy", function (d) { return y(d); } ) // translate y value to a pixel
-           .attr("cx", function (d,i) { return x(xdata[i]); } ) // translate x value
-           .attr("r", 5) // radius of circle
-           .style("opacity", 0.6); // opacity of circle
-           //Load the dataset from data.csv using xAxis and yAxis variables
-    });
+           var plot = svg.append("g")                                   //Append Scatter Plot
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .attr("width", width)
+                .attr("height", height)
+                .attr("class", "main");
+           
+           plot.append("g")                                             //Append Axes to Scatter Plot
+                .attr("transform", "translate(0," + height + ")")
+                .attr("class", "main axis date")
+                .call(xaxis)
+            .append("text")
+                .attr("x", width)
+                .attr("dy", "-0.4em")
+                .style("text-anchor", "end")
+                .text(xAxis);
+           plot.append("g")
+                .attr("transform", "translate(0,0)")
+                .attr("class", "main axis date")
+                .call(yaxis)
+           .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "0.71em")
+                .style("text-anchor", "end")
+                .text(yAxis);
+           
+           var legend = plot.append("g")                                //Append Legend
+                .attr("class", "legend")
+                .attr("x", width)
+                .attr("y", 25)
+                .attr("height", 100)
+                .attr("width", 100);
+           for(var i = 0; i < 3; i++) {
+               legend.append("text")
+                    .attr("x", width - 50)
+                    .attr("y", 25*i)
+                    .text(function(d) { return varities[i]; });
+               legend.append("rect")
+                    .attr("x", width)
+                    .attr("y", 25*i)
+                    .attr("width", 10)
+                    .attr("height", 10)
+                    .style("fill", function(d) { return chooseColor(varities[i]); });
+           }
+           
+           plot.append("g")                                             //Plot data
+                .selectAll("points")
+                .data(xData).enter()
+               .append("circle")
+                    .attr("cy", function(d,i) { return yScale(yData[i]); })
+                    .attr("cx", function(d,i) { return xScale(xData[i]); })
+                    .attr("r", 3)
+                    .attr("fill", function(d,i) { return chooseColor(vData[i]); })
+                    .style("opacity", 0.5)
+           .on("mouseover", function(d,i){ displayData(i); });
+           });
+}
+
+function chooseColor(v) {
+    if(v == varities[0]) return "blue";
+    else if(v == varities[1]) return "orange";
+    else return "green";
+}
+
+function displayData(i) {
+    d3.select("#details").select("text").remove();
+    d3.select("#details")
+        .append("text").html("Compactness: " + cData[i] + "</br>")
+        .append("text").html("Kernel Length: " + klData[i] + "</br>")
+        .append("text").html("Kernel Width: " + kwData[i] + "</br>")
+        .append("text").html("Asymmetry Coefficient: " + aData[i] + "</br>")
+        .append("text").html("Groove Length: " + gData[i] + "</br>")
+        .append("text").html("Variety: " + vData[i] + "</br>") ;
 }
 
 /**
-## onXAxisChange(value)
-This function is called whenever the menu for the variable to display on the
-x axis changes. It is passed the variable name that has been selected, such as
-"compactness". Populate this function to update the scatterplot accordingly.
-**/
+ ## onXAxisChange(value)
+ This function is called whenever the menu for the variable to display on the
+ x axis changes. It is passed the variable name that has been selected, such as
+ "compactness". Populate this function to update the scatterplot accordingly.
+ **/
 function onXAxisChange(value){
-    console.log("on x axis change");
-    init(value, yAxisData);
+    init(value, y);
 }
 
 
 /**
-## onYAxisChange(value)
-This function is called whenever the menu for the variable to display on the
-y axis changes. It is passed the variable name that has been selected, such as
-"Asymmetry Coefficient". Populate this function to update the scatterplot 
-accordingly.
-**/
+ ## onYAxisChange(value)
+ This function is called whenever the menu for the variable to display on the
+ y axis changes. It is passed the variable name that has been selected, such as
+ "Asymmetry Coefficient". Populate this function to update the scatterplot
+ accordingly.
+ **/
 function onYAxisChange(value){
-    console.log("on y axis change");
-    init(xAxisData, value);
+    init(x, value);
 }
 
 /**
-## showDetails(string)
-This function will display details in the "details" box on the page. Pass in 
-a string and it will be displayed. For example, 
-    showDetails("Variety: " + item.variety);
-**/
+ ## showDetails(string)
+ This function will display details in the "details" box on the page. Pass in
+ a string and it will be displayed. For example,
+ showDetails("Variety: " + item.variety);
+ **/
 function showDetails(string){
     d3.select('#details').html(string);
 }
