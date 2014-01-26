58e9b5a1ee289d5a8f084ea9b909b56c5ed6045e
Alexander Ikonomidis <xikonomidis@gmail.com>
2014-01-26 14:21:52 -0500

    Plotting now!!!

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-var svg;
 /**
 THIS FUNCTION IS CALLED WHEN THE WEB PAGE LOADS. PLACE YOUR CODE TO LOAD THE
 DATA AND DRAW YOUR VISUALIZATION HERE. THE VIS SHOULD BE DRAWN INTO THE "VIS" 
@@ -7,88 +6,80 @@ DIV ON THE PAGE.
 This function is passed the variables to initially draw on the x and y axes.
 **/
 function init(xAxis, yAxis){
-   
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-    
-    var xScale = d3.scale.linear()
-    .range([0, width]);
-    
-    var yScale = d3.scale.linear()
-    .range([height, 0]);
-    
-    var xaxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom")
-    .ticks(10);
-    
-    var yaxis = d3.svg.axis()
-    .scale(yScale)
-    .orient("left")
-    .ticks(10);
-    
-    svg = d3.select("#vis").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
     var dataset = [];
     d3.csv("data/data.csv", function(data) {
            dataset = data.map(function(d) { return [ +d[xAxis], +d[yAxis] ]; });
-           console.log(dataset[0]);
-           xScale.domain(data.map(function(d) { return d.letter; }));
-           yScale.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+           var xdata = data.map(function(d){ return +d[xAxis];});
+           var ydata = data.map(function(d){ return +d[yAxis];});
+           console.log(dataset);
            
-           svg.append("g")
-           .attr("class", "x axis")
-           .attr("transform", "translate(0," + height + ")")
-           .call(xaxis)
-           .append("text")
-           .attr("y", -10)
-           .attr("dy", ".71em")
-           .attr("x", width)
-           .attr("dx", "0em")
-           .style("text-anchor", "end")
-           .text(xAxis);
+           // data that you want to plot, I've used separate arrays for x and y values
+           //var xdata = [5, 10, 15, 20], ydata = [3, 17, 4, 6];
            
-           svg.append("g")
-           .attr("class", "y axis")
-           .call(yaxis)
-           .append("text")
-           .attr("transform", "rotate(-90)")
-           .attr("y", 6)
-           .attr("dy", ".71em")
-           .style("text-anchor", "end")
-           .text(yAxis);
+           console.log(xdata);
+           console.log(ydata);
            
-           //Create circles
-           svg.selectAll("circle")
-           .data(dataset)
-           .enter()
-           .append("circle")
-           .attr("cx", function(d) {
-                 return xScale(d[0]);
-                 })
-           .attr("cy", function(d) {
-                 return yScale(d[1]);
-                 });
+           // size and margins for the chart
+           var margin = {top: 20, right: 15, bottom: 60, left: 60}
+           , width = 960 - margin.left - margin.right
+           , height = 500 - margin.top - margin.bottom;
            
-           //Create X axis
-           svg.append("g")
-           .attr("class", "axis")
-           .attr("transform", "translate(0," + height + ")")
+           // x and y scales, I've used linear here but there are other options
+           // the scales translate data values to pixel values for you
+           var x = d3.scale.linear()
+           .domain([d3.min(xdata), d3.max(xdata)])  // the range of the values to plot
+           .range([ 0, width ]);        // the pixel range of the x-axis
+           
+           var y = d3.scale.linear()
+           .domain([d3.min(ydata), d3.max(ydata)])
+           .range([ height, 0 ]);
+           
+           // the chart object, includes all margins
+           var chart = d3.select('#vis')
+           .append('svg:svg')
+           .attr('width', width + margin.right + margin.left)
+           .attr('height', height + margin.top + margin.bottom)
+           .attr('class', 'chart')
+           
+           // the main object where the chart and axis will be drawn
+           var main = chart.append('g')
+           .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+           .attr('width', width)
+           .attr('height', height)
+           .attr('class', 'main')
+           
+           // draw the x axis
+           var xaxis = d3.svg.axis()
+           .scale(x)
+           .orient('bottom');
+           
+           main.append('g')
+           .attr('transform', 'translate(0,' + height + ')')
+           .attr('class', 'main axis date')
            .call(xaxis);
            
-           //Create Y axis
-           svg.append("g")
-           .attr("class", "axis")
-           .attr("transform", "translate(" + 0 + ",0)")
+           // draw the y axis
+           var yaxis = d3.svg.axis()
+           .scale(y)
+           .orient('left');
+           
+           main.append('g')
+           .attr('transform', 'translate(0,0)')
+           .attr('class', 'main axis date')
            .call(yaxis);
            
-           });
-    //Load the dataset from data.csv using xAxis and yAxis variables
+           // draw the graph object
+           var g = main.append("svg:g");
+           
+           g.selectAll("scatter-dots")
+           .data(ydata)  // using the values in the ydata array
+           .enter().append("svg:circle")  // create a new circle for each value
+           .attr("cy", function (d) { return y(d); } ) // translate y value to a pixel
+           .attr("cx", function (d,i) { return x(xdata[i]); } ) // translate x value
+           .attr("r", 5) // radius of circle
+           .style("opacity", 0.6); // opacity of circle
+           //Load the dataset from data.csv using xAxis and yAxis variables
+    });
 }
 
 /**
@@ -111,7 +102,8 @@ y axis changes. It is passed the variable name that has been selected, such as
 accordingly.
 **/
 function onYAxisChange(value){
-
+    console.log("on x axis change");
+    init(xdata, value);
 }
 
 /**
--- js/main_ex.js
+++ js/main_ex.js
@@ -1,110 +1,63 @@
+// data that you want to plot, I've used separate arrays for x and y values
+var xdata = [5, 10, 15, 20],
+ydata = [3, 17, 4, 6];
 
+// size and margins for the chart
+var margin = {top: 20, right: 15, bottom: 60, left: 60}
+, width = 960 - margin.left - margin.right
+, height = 500 - margin.top - margin.bottom;
 
+// x and y scales, I've used linear here but there are other options
+// the scales translate data values to pixel values for you
+var x = d3.scale.linear()
+.domain([0, d3.max(xdata)])  // the range of the values to plot
+.range([ 0, width ]);        // the pixel range of the x-axis
 
-//Width and height
-var w = 500;
-var h = 300;
-var padding = 30;
+var y = d3.scale.linear()
+.domain([0, d3.max(ydata)])
+.range([ height, 0 ]);
 
-/*
- //Static dataset
- var dataset = [
- [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
- [410, 12], [475, 44], [25, 67], [85, 21], [220, 88],
- [600, 150]
- ];
- */
+// the chart object, includes all margins
+var chart = d3.select('body')
+.append('svg:svg')
+.attr('width', width + margin.right + margin.left)
+.attr('height', height + margin.top + margin.bottom)
+.attr('class', 'chart')
 
-//Dynamic, random dataset
-var dataset = [];					//Initialize empty array
-var numDataPoints = 50;				//Number of dummy data points to create
-var xRange = Math.random() * 1000;	//Max range of new x values
-var yRange = Math.random() * 1000;	//Max range of new y values
-for (var i = 0; i < numDataPoints; i++) {					//Loop numDataPoints times
-    var newNumber1 = Math.round(Math.random() * xRange);	//New random integer
-    var newNumber2 = Math.round(Math.random() * yRange);	//New random integer
-    dataset.push([newNumber1, newNumber2]);					//Add new number to array
-}
+// the main object where the chart and axis will be drawn
+var main = chart.append('g')
+.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+.attr('width', width)
+.attr('height', height)
+.attr('class', 'main')
 
-//Create scale functions
-var xScale = d3.scale.linear()
-.domain([0, d3.max(dataset, function(d) { return d[0]; })])
-.range([padding, w - padding * 2]);
-
-var yScale = d3.scale.linear()
-.domain([0, d3.max(dataset, function(d) { return d[1]; })])
-.range([h - padding, padding]);
-
-var rScale = d3.scale.linear()
-.domain([0, d3.max(dataset, function(d) { return d[1]; })])
-.range([2, 5]);
-
-var formatAsPercentage = d3.format(".1%");
-
-//Define X axis
+// draw the x axis
 var xAxis = d3.svg.axis()
-.scale(xScale)
-.orient("bottom")
-.ticks(5)
-.tickFormat(formatAsPercentage);
+.scale(x)
+.orient('bottom');
 
-//Define Y axis
-var yAxis = d3.svg.axis()
-.scale(yScale)
-.orient("left")
-.ticks(5)
-.tickFormat(formatAsPercentage);
-
-//Create SVG element
-var svg = d3.select("#vis")
-.append("svg")
-.attr("width", w)
-.attr("height", h);
-
-//Create circles
-svg.selectAll("circle")
-.data(dataset)
-.enter()
-.append("circle")
-.attr("cx", function(d) {
-      return xScale(d[0]);
-      })
-.attr("cy", function(d) {
-      return yScale(d[1]);
-      })
-.attr("r", function(d) {
-      return rScale(d[1]);
-      });
-
-/*
- //Create labels
- svg.selectAll("text")
- .data(dataset)
- .enter()
- .append("text")
- .text(function(d) {
- return d[0] + "," + d[1];
- })
- .attr("x", function(d) {
- return xScale(d[0]);
- })
- .attr("y", function(d) {
- return yScale(d[1]);
- })
- .attr("font-family", "sans-serif")
- .attr("font-size", "11px")
- .attr("fill", "red");
- */
-
-//Create X axis
-svg.append("g")
-.attr("class", "axis")
-.attr("transform", "translate(0," + (h - padding) + ")")
+main.append('g')
+.attr('transform', 'translate(0,' + height + ')')
+.attr('class', 'main axis date')
 .call(xAxis);
 
-//Create Y axis
-svg.append("g")
-.attr("class", "axis")
-.attr("transform", "translate(" + padding + ",0)")
+// draw the y axis
+var yAxis = d3.svg.axis()
+.scale(y)
+.orient('left');
+
+main.append('g')
+.attr('transform', 'translate(0,0)')
+.attr('class', 'main axis date')
 .call(yAxis);
 
+// draw the graph object
+var g = main.append("svg:g");
+
+g.selectAll("scatter-dots")
+.data(ydata)  // using the values in the ydata array
+.enter().append("svg:circle")  // create a new circle for each value
+.attr("cy", function (d) { return y(d); } ) // translate y value to a pixel
+.attr("cx", function (d,i) { return x(xdata[i]); } ) // translate x value
+.attr("r", 10) // radius of circle
+.style("opacity", 0.6); // opacity of circle
